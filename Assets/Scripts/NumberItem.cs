37ac52b815f0d2b66d8d0210f21ed5db3c540fbb
FelipePipeArango <feliae.98@gmail.com>
2025-03-11 17:36:27 -0400

    ShaderFix

--- Assets/Scripts/NumberItem.cs
+++ Assets/Scripts/NumberItem.cs
@@ -17,16 +17,28 @@ public class NumberItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDr
     public static event DragActions OnDragAction;
 
     public Transform target;  // The target object the player or item approaches
-    
+
+    float lockedPosition = 0f;
+    [SerializeField] Camera UiCamera;
+    public Vector3 mousePosition;
+    public Vector3 world;
+    public Canvas canvas;
+
+    private RectTransform thisRectTransform;
+
     void Awake()
     {
         numberText.text = value.ToString();
+        thisRectTransform = GetComponent<RectTransform>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         parentTransform = transform.parent;
         transform.SetParent(transform.root);
+        /*Vector3 currentPosition = transform.localPosition;
+        currentPosition.z = lockedPosition;
+        transform.localPosition = currentPosition;*/
         image.raycastTarget = false;
         transform.SetAsLastSibling();
 
@@ -38,7 +50,32 @@ public class NumberItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDr
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = Input.mousePosition;
+        /*mousePosition = Input.mousePosition;
+        mousePosition.z = lockedPosition;
+        world = UiCamera.ScreenToWorldPoint(mousePosition);
+        //world.z = lockedPosition;
+        transform.position = UiCamera.ScreenToWorldPoint(mousePosition);
+        //transform.position = Input.mousePosition;*/
+
+        Vector2 localPosition;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle (canvas.transform as RectTransform, eventData.position, UiCamera, out localPosition);
+
+       mousePosition = eventData.position;
+        mousePosition.z = UiCamera.nearClipPlane;  // Set the Z position to the near clip plane (or a fixed distance)
+
+        // Convert the screen space position to world space
+        Vector3 worldPos = UiCamera.ScreenToWorldPoint(mousePosition);
+
+        Vector3 offset = Vector3.zero;
+
+        // Calculate the offset from the original object position to the mouse position
+        if (offset == Vector3.zero)
+        {
+            offset = transform.position - worldPos;
+        }
+
+        // Update the object's position in world space
+        thisRectTransform.localPosition = localPosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
