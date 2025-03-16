022d71cb4b7742b9cdaeb722bafb64f3fdd12b44
InfectedCosta <JustinCosta4242@gmail.com>
2025-03-15 21:56:47 -0400

    Cursor system started.
    -Added Cursor PNG's (Placeholders)
    -Added Start of cursor system.

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,22 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+public enum CursorState
+{
+    Default,
+    Interact,
+    Holding
+}
+
 public class UIHandler : MonoBehaviour
 {
-   
     public TMP_Text numberText;
+    public Texture2D defaultCursor;
+    public Texture2D interactCursor;
+    public Texture2D holdingCursor;
+    public Vector2 cursorHotspot = Vector2.zero;
 
+    void Start()
+    {
+        UpdateNumberText(0);
+        SetCursorState(CursorState.Default);
+    }
 
     public void UpdateNumberText(int numberValue)
     {
         numberText.text = numberValue.ToString();
     }
 
-    void Start()
+    public void SetCursorState(CursorState state)
     {
-        UpdateNumberText(0);
+        switch (state)
+        {
+            case CursorState.Default:
+                Cursor.SetCursor(defaultCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case CursorState.Interact:
+                Cursor.SetCursor(interactCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case CursorState.Holding:
+                Cursor.SetCursor(holdingCursor, cursorHotspot, CursorMode.Auto);
+                break;
+        }
     }
 }
+
+
+
+
+
 
