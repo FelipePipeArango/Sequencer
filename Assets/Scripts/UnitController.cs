389432411d05264494d89b7d10aa80de0cba148c
Dermo03 <Vladislav.Tsyao@georgebrown.ca>
2025-03-23 11:07:26 -0400

    [MOD] UnitController.cs
    Added new function for the complete screen that waits 5 second till making it all go to main menu no buttons implemented

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -8,6 +8,10 @@ using static GridManager;
 
 public class UnitController : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject CardsInLevel;
+    public GameObject NumberSlot   ;
+    public GameObject CompleteLevel;
 
     [HideInInspector] public float fallSpeed = 1.0f;
     [HideInInspector] public bool hasItem = false;
@@ -111,12 +115,42 @@ public class UnitController : MonoBehaviour
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
 
-        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
-        {
-            SceneManager.LoadScene(nextSceneIndex);
-        }
+        //if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        //{
+        //    SceneManager.LoadScene(nextSceneIndex);
+        //}
         Debug.Log("GOAL");
+        //TODO Add functional for this function
+        //make it so when triggerred makes the completelevel active and everything else disabled 
+        StartCoroutine(CompleteScreen(1));
     }
+
+    IEnumerator CompleteScreen(int count)
+    {
+        if (count < 0)
+        {
+          yield return null;
+        }
+        if (count == 1)
+        {
+            CardsInLevel.GameObject().SetActive(false);
+            NumberSlot.GameObject().SetActive(false);
+            CompleteLevel.GameObject().SetActive(true);
+
+            yield return new WaitForSeconds(5.0f);
+        }
+        else if(count == 0)
+        {
+            MenuScreen();
+        }
+        StartCoroutine(CompleteScreen(count-1));
+    }
+    void MenuScreen()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
 
     protected void KeyItemCheck()
     {
