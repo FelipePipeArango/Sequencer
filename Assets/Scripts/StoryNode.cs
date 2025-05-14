4e62f0a4eba53d881ff7654583758100cedc557a
MZS27 <54246982+MZS27@users.noreply.github.com>
2025-05-14 13:02:15 -0400

    Minor fixes

--- Assets/Scripts/StoryNode.cs
+++ Assets/Scripts/StoryNode.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "StoryNode")]
 public class StoryNode : ScriptableObject
 {
+    [Header("Add story text here. The text will be displayed at runtime.")]
     [TextArea(2, 10)] public string storyText;
+    
+    [Header("Add story options here. These options will be visible at runtime. The 'Next Node' wil contain the StoryNode object that will be displayed when the players selects a particular option.")]
     public List<StoryOption> options;
 }
 
